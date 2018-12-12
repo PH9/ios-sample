a39be7a0fb138ab64f0ecb2ef2da02748716b07e
Wasith Theerapattrathamrong <phai@live.com>
2018-12-12 11:19:07 +0700

    Initial Commit

---
+++ Sample/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Sample
+//
+//  Created by Wasith Theerapattrathamrong on 12/12/2561 BE.
+//  Copyright © 2561 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Sample/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Sample
+//
+//  Created by Wasith Theerapattrathamrong on 12/12/2561 BE.
+//  Copyright © 2561 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
